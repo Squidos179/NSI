ae11eacadcea5ae78325f252a762b3d4feafad49
Squidos179 <henri457.aubree@gmail.com>
2023-10-10 11:46:17 +0200

    g con tinu è

--- Projet_Web_SQL/AFVM4.py
+++ Projet_Web_SQL/AFVM4.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
-import datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    date = datetime.datetime.now()
-    h = date.hour
-    m = date.minute
-    s = date.second
-    return render_template("index.html", heure = h, minute = m, seconde = s)
+    return render_template("index.html")
+@app.route('/resultat', methods = ['POST'])
+def resultat():
+    result = request.form
+    n = result['nom']
+    p = result['prenom']
+    return render_template("resultat.html", nom=n, prenom=p)
 app.run(debug=True)
