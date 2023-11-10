83ec6ab5abf4df62a1be86214e08cbc08d7d0f2a
Squidos179 <henri457.aubree@gmail.com>
2023-11-10 15:05:19 +0100

    Finition des Docstring

--- CHAP5/Vecteur/vector.py
+++ CHAP5/Vecteur/vector.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from math import sqrt
 
 class Vector2D:
@@ -5,7 +6,6 @@ class Vector2D:
         """
         Constructeur de la class Vector2D
         Args:
-            self: l'objet créé
             x : un décimal
             y: un décimal
         Return:
@@ -18,8 +18,6 @@ class Vector2D:
     def norm(self):
         """
         Donne la norme d'un vecteur
-        Args:
-            self : un objet de type Vector2D
         Return:
             Le calcul de la norme du vecteur donné en paramètre
         """
@@ -29,7 +27,6 @@ class Vector2D:
         """
         Multiplie un vecteur par un coefficient
         Args:
-            self : un objet de type Vector2D
             coefficient : un entier
         Return:
             Ne retourne rien car modifie directement le vecteur donné
@@ -38,28 +35,82 @@ class Vector2D:
         self.y *= coefficient
 
     def add(self, vector):
+        """
+        Ajoute à un vecteur un autre vecteur
+        Args:
+            self : un objet de type Vector2D auquel on va ajouter un autre Vector2D
+        Return:
+            Ne retourne rien car modifie directement le vecteur donné
+        """
         self.x += vector.x
         self.y += vector.y
 
     def scalarProduct(self, vector):
+        """
+        Calcul le produit scalaire de deux vecteur
+        Args:
+            vector : Un objet de type Vector2D
+        Return:
+            Retourne un décimal qui est le produit scalaire du vecteur et d'un autre vecteur
+        """
         return self.x*vector.x + self.y*vector.y
     
     def perpendicular(self, vector):
+        """
+        Permet de savoir le vecteur et un autre vecteur sont perpendiculaire
+        Args:
+            vector : Un objet de type Vector2D
+        Return:
+            Retourne True si les deux vecteurs sont perpendiculaires et False si ce n'est pas le cas
+        """
         return self.scalarProduct(vector) == 0
     
     def colinear(self, vector):
+        """
+        Permet de savoir le vecteur et un autre vecteur sont colinéaire
+        Args:
+            vector : Un objet de type Vector2D
+        Return:
+            Retourne True si les deux vecteurs sont colinéaires et False si ce n'est pas le cas
+        """
         return self.x * vector.y - self.y * vector.x == 0
     
     def __mul__(self, vector):
+        """
+        Permet de gérer un cas de multiplication de deux vecteurs
+        Args:
+            vector : Un objet de type Vector2D
+        Return:
+            Retourne un décimal qui est le résultat de la fonction "scalarProduct" avec en paramètre 'vector'
+        """
         return self.scalarProduct(vector)
     
     def __add__(self, vector):
+        """
+        Permet de gérer un cas d'addition de deux vecteurs
+        Args:
+            vector : Un objet de type Vector2D
+        Return:
+            Retourne un nouvel objet de type Vector2D avec en attribut l'addition de deux vecteurs
+        """
         return Vector2D(self.x + vector.x, self.y + vector.y)
     
     def __repr__(self):
+        """
+        Represente le vecteur avec un string
+        
+        Return:
+            Retourne un string qui donne les valeurs du vecteur
+        """
         return f'Vecteur de coordonnées x : {self.x}, y:{self.y}'
     
     def __str__(self) -> str:
+        """
+        Ce que retourne un print() avec un vecteur en argument
+
+        Return:
+            Retourne la même chose que le __repr__ donc un string qui donne les valeurs du vecteur
+        """
         return self.__repr__()
     
 v1 = Vector2D(3, 4)
