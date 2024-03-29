91651879f274321d90162d3549db9750b101857b
squidos179 <henri457.aubree@gmail.com>
2024-03-29 07:33:29 +0100

    Je sais pas ce que j'ai fait

--- Snake/Final/anneau.py
+++ Snake/Final/anneau.py
@@ -4,7 +4,13 @@ from random import randint
 class Anneau:
 
     def __init__(self, avant = None) -> None:
-
+        """
+        Initialise un objet Anneau.
+        Args:
+            avant (Anneau): Anneau précédent, par défaut égal à None pour quand on crée la tête.
+        Returns:
+            None : on crée un objet Anneau.
+        """
         self.x = -1000
         self.y = -1000
         self.fx = 0
@@ -13,20 +19,42 @@ class Anneau:
         self.avant = avant
 
     def update(self):
+        """
+        Met à jour les coordonnées de l'anneau en fonction des nouvelles coordonnées fournies.
+        Si l'anneau a un anneau suivant, met également à jour les coordonnées de l'anneau suivant.
+        Returns:
+            None : on modifie directement les attributs de l'anneau.
+        """
         self.x = self.fx
         self.y = self.fy
 
         if self.suivant != None:
-            
             self.suivant.update()
 
     def render(self, surf, color):
+        """
+        Dessine l'anneau sur la surface donnée.
+        Args:
+            surf (pygame.Surface): La surface sur laquelle on dessine.
+            color (tuple): La couleur de l'anneau.
+        Returns:
+            None : on modifie directement la surface.
+        """
         pygame.draw.rect(surf, color, (self.x, self.y, 20, 20))
         if self.suivant != None:
             self.suivant.render(surf, color)
 
     def ajout(self):
+        """
+        Ajoute un nouvel anneau à la suite de l'anneau actuel.
+
+        Si l'anneau suivant est None, crée un nouvel anneau et le définit comme l'anneau suivant.
+        Sinon, appelle récursivement la méthode ajout sur l'anneau suivant.
+
+        Returns:
+            None : on modifie directement les attributs des anneaux.
+        """
         if self.suivant == None:
             self.suivant = Anneau(self)
         else:
-            return self.suivant.ajout()
+            self.suivant.ajout()
--- Snake/Final/snake2.py
+++ Snake/Final/snake2.py
@@ -4,9 +4,19 @@ import pygame
 class Serpent2(Serpent):
 
     def __init__(self) -> None:
+        """
+        Initialise un objet Serpent2 en utlisant comme base la classe Serpent avec de l'héritage.
+        Returns:
+            None : on crée un objet Serpent2.
+        """
         super().__init__()
 
     def keyHandling(self):
+        """
+        Gère les touches du clavier pour contrôler le serpent
+        Returns:
+            None : on modifie directement les attributs du serpent.
+        """
         keys = pygame.key.get_pressed()
         if keys[pygame.K_q] and self.vec2D[0] != 1:
             self.vec2D[0] = -1
@@ -22,6 +32,14 @@ class Serpent2(Serpent):
             self.vec2D[1] = 1
 
     def render(self, surf, scoreFont):
+        """
+        Dessine le serpent sur la surface donnée.
+        Args:
+            surf (pygame.Surface): La surface sur laquelle on dessine.
+            scoreFont (pygame.font.Font): La police pour afficher le score.
+        Returns:
+            None : on modifie directement la surface.
+        """
         self.head.render(surf, (0x7B, 0x50, 0x6F))
 
         scoreText = scoreFont.render(str(self.score), True, (255, 255, 255))
