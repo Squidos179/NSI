3d7bae3be2c60bfe8fa61197b95e097714f027e7
Squidos <henri457.aubree@gmaiL.com>
2024-04-02 20:25:39 +0200

    Exo

--- CHAP13/EX7P328.py
+++ CHAP13/EX7P328.py
@@ -1,2 +1,14 @@
 def count_change(n, k ,pieces):
-    pass
+    cache = {}
+    if n < 0:
+        return 0
+    if (n, k) in cache:
+        return cache[n, k]
+    if k == 0:
+        v = 1
+    else:
+        v = count_change(n - pieces[k], k, pieces) + count_change(n, k -1, pieces)
+    cache[n, k] = v
+    return v
+
+#En n*k car on converse tout les résultats précédants
